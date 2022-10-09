17e35191b4fb0237ffd0054ae10e31373d874591
mimron <muhammadimron39@gmail.com>
2022-10-10 04:28:28 +0700

    fix-query-member-report

--- routes/members.js
+++ routes/members.js
@@ -1,6 +1,8 @@
 const express = require("express");
+
 const router = express.Router();
 const Member = require("../models").Member;
+const sequelize = require("../models").sequelize;
 const passport = require("passport");
 const excel = require("exceljs");
 require("../config/passport")(passport);
@@ -197,57 +199,68 @@ router.delete(
 // Get Member downnload
 router.get(
   "/download/excel",
-//   passport.authenticate("jwt", {
-//     session: false,
-//   }),
+  //   passport.authenticate("jwt", {
+  //     session: false,
+  //   }),
   function (req, res) {
-    Member.findAll()
-          .then((members) => {
-            // res.status(200).send(members)
-            let datas = [];
+    sequelize
+      .query(
+        `SELECT 
+    t1.rep_id as mst_gepd, t1.name as name_gepd, 
+    t2.rep_id as mst_epd, t2.name as name_epd, 
+    t3.branch_id, t3.name as name
+    FROM Members AS t1
+    LEFT JOIN Members AS t2 ON t2.manager_id = t1.rep_id
+    LEFT JOIN Members AS t3 ON t3.manager_id = t2.rep_id
+    WHERE t3.name IS NOT NULL`,
+        { raw: true }
+      )
+      .then((results) => {
+        // res.status(200).send(members)
+        let datas = [];
 
-            members.forEach((obj) => {
-                datas.push({
-                id: obj.id,
-                branch_id: obj.branch_id,
-                rep_id: obj.rep_id,
-                name: obj.name,
-                current_position: obj.current_position,
-                manager_id: obj.manager_id,
-              });
-            });
-
-            let workbook = new excel.Workbook();
-            let worksheet = workbook.addWorksheet("Data Member");
-
-            worksheet.columns = [
-              { header: "Id", key: "id", width: 5 },
-              { header: "Branch", key: "branch_id", width: 25 },
-              { header: "Rep", key: "rep_id", width: 25 },
-              { header: "Name", key: "name", width: 10 },
-              { header: "Position", key: "current_position", width: 10 },
-              { header: "Manager", key: "manager_id", width: 10 },
-            ];
-
-            // Add Array Rows
-            worksheet.addRows(datas);
-
-            res.setHeader(
-              "Content-Type",
-              "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-            );
-            res.setHeader(
-              "Content-Disposition",
-              "attachment; filename=" + "members.xlsx"
-            );
-
-            return workbook.xlsx.write(res).then(function () {
-              res.status(200).end();
-            });
-          })
-          .catch((error) => {
-            res.status(400).send(error);
+        results[0].forEach((obj) => {
+          datas.push({
+            mst_gepd: obj.mst_gepd,
+            name_gepd: obj.name_gepd,
+            mst_epd: obj.mst_epd,
+            name_epd: obj.name_epd,
+            branch_id: obj.branch_id,
+            name: obj.name,
           });
+        });
+
+        let workbook = new excel.Workbook();
+        let worksheet = workbook.addWorksheet("Data Member");
+
+        worksheet.columns = [
+          { header: "mst_gepd", key: "mst_gepd", width: 25 },
+          { header: "name_gepd", key: "name_gepd", width: 25 },
+          { header: "mst_epd", key: "mst_epd", width: 25 },
+          { header: "name_epd", key: "name_epd", width: 25 },
+          { header: "branch_id", key: "branch_id", width: 25 },
+          { header: "name", key: "name", width: 25 },
+        ];
+
+        // Add Array Rows
+        worksheet.addRows(datas);
+
+        res.setHeader(
+          "Content-Type",
+          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        );
+        res.setHeader(
+          "Content-Disposition",
+          "attachment; filename=" + "members.xlsx"
+        );
+
+        return workbook.xlsx.write(res).then(function () {
+          res.status(200).end();
+        });
+      })
+      .catch((error) => {
+        res.status(400).send(error);
+      });
   }
 );
 
